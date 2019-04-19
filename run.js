e1c42a391445c06510490296b589b54ee73869ed
ecnologia <ecnologiasolutions@gmail.com>
2019-04-19 03:18:28 +0200

    impostor

---
+++ run.js
@@ -0,0 +1,236 @@
+var express = require('express');
+var app = express();
+var serv = require ('http').Server(app);
+
+app.get('/',function(req, res){
+  res.sendFile(__dirname + '/client/index.html');
+})
+app.use('/client', express.static(__dirname + '/client'));
+serv.listen (8001);
+console.log(`empezamos letsgo`)
+console.log('puerto 8001')
+
+
+var SOCKET_LIST = {};
+
+var Entity = function(){
+	var self = {
+		x:250,
+		y:250,
+		spdX:0,
+		spdY:0,
+		id:"",
+	}
+	self.update = function(){
+		self.updatePosition();
+	}
+	self.updatePosition = function(){
+		self.x += self.spdX;
+		self.y += self.spdY;
+	}
+	self.getDistance = function(pt){
+		return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+	}
+	return self;
+}
+
+var Player = function(id){
+	var self = Entity();
+	self.id = id;
+	self.number = "" + Math.floor(10 * Math.random());
+	self.pressingRight = false;
+	self.pressingLeft = false;
+	self.pressingUp = false;
+	self.pressingDown = false;
+	self.pressingAttack = false;
+	self.mouseAngle = 0;
+	self.maxSpd = 10;
+
+	var super_update = self.update;
+	self.update = function(){
+		self.updateSpd();
+		super_update();
+
+		if(self.pressingAttack){
+			self.shootBullet(self.mouseAngle);
+		}
+	}
+	self.shootBullet = function(angle){
+		var b = Bullet(self.id,angle);
+		b.x = self.x;
+		b.y = self.y;
+	}
+
+	self.updateSpd = function(){
+		if(self.pressingRight)
+			self.spdX = self.maxSpd;
+		else if(self.pressingLeft)
+			self.spdX = -self.maxSpd;
+		else
+			self.spdX = 0;
+
+		if(self.pressingUp)
+			self.spdY = -self.maxSpd;
+		else if(self.pressingDown)
+			self.spdY = self.maxSpd;
+		else
+			self.spdY = 0;
+	}
+	Player.list[id] = self;
+
+	initPack.player.push({
+		id:self.id,
+		x:self.x,
+		y:self.y,
+		number:self.number,
+	});
+	return self;
+}
+Player.list = {};
+Player.onConnect = function(socket){
+	var player = Player(socket.id);
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		else if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
+		else if(data.inputId === 'attack')
+			player.pressingAttack = data.state;
+		else if(data.inputId === 'mouseAngle')
+			player.mouseAngle = data.state;
+	});
+}
+Player.onDisconnect = function(socket){
+	delete Player.list[socket.id];
+	removePack.player.push(socket.id);
+}
+Player.update = function(){
+	var pack = [];
+	for(var i in Player.list){
+		var player = Player.list[i];
+		player.update();
+		pack.push({
+			id:player.id,
+			x:player.x,
+			y:player.y,
+		});
+	}
+	return pack;
+}
+
+
+var Bullet = function(parent,angle){
+	var self = Entity();
+	self.id = Math.random();
+	self.spdX = Math.cos(angle/180*Math.PI) * 10;
+	self.spdY = Math.sin(angle/180*Math.PI) * 10;
+	self.parent = parent;
+	self.timer = 0;
+	self.toRemove = false;
+	var super_update = self.update;
+	self.update = function(){
+		if(self.timer++ > 100)
+			self.toRemove = true;
+		super_update();
+
+		for(var i in Player.list){
+			var p = Player.list[i];
+			if(self.getDistance(p) < 32 && self.parent !== p.id){
+				//handle collision. ex: hp--;
+				self.toRemove = true;
+			}
+		}
+	}
+	Bullet.list[self.id] = self;
+	initPack.bullet.push({
+		id:self.id,
+		x:self.x,
+		y:self.y,
+	});
+	return self;
+}
+Bullet.list = {};
+
+Bullet.update = function(){
+	var pack = [];
+	for(var i in Bullet.list){
+		var bullet = Bullet.list[i];
+		bullet.update();
+		if(bullet.toRemove){
+			delete Bullet.list[i];
+			removePack.bullet.push(bullet.id);
+		} else
+			pack.push({
+				id:bullet.id,
+				x:bullet.x,
+				y:bullet.y,
+			});
+	}
+	return pack;
+}
+
+
+
+var initPack = {player:[],bullet:[]};
+var removePack = {player:[],bullet:[]};
+
+
+setInterval(function(){
+	var pack = {
+		player:Player.update(),
+		bullet:Bullet.update(),
+	}
+
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('init',initPack);
+		socket.emit('update',pack);
+		socket.emit('remove',removePack);
+	}
+	initPack.player = [];
+	initPack.bullet = [];
+	removePack.player = [];
+	removePack.bullet = [];
+
+},1000/25);
+/*
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+
+    socket.on('happy',function(data){
+        console.log('happy because ' + data.reason);
+    });
+
+    socket.emit('serverMsg',{
+        msg:'hello',
+    });
+
+});
+
+
+
+/*
+//#con http va k
+const http = require('http')
+
+const port = 8001
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})*/
